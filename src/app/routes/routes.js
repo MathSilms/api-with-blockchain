2be46fad362c678cd787ca12dbc60c4ce1100db6
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-30 17:53:25 -0300

    instanciando a blockchain e o server no routes

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -1,12 +1,24 @@
 const Router = require("express").Router;
+const Blockchain = require('../../blockchain/index')
+const P2pServer = require('../p2p-server');
 
 // controllers
 
 const BlockController = require("../controllers/block.controller");
 
+
+// instâncias
+
+const bc = new Blockchain();
+const p2pServer = new P2pServer(bc);
+
+let blockController = new BlockController(bc, p2pServer);
 const router = new Router();
 
-router.get('/blocks', BlockController.findBlocks);
-router.post('/mineBlock', BlockController.mineBlock);
+// rotas
 
-module.exports = router
+router.get('/blocks', blockController.findBlocks);
+router.post('/mineBlock', blockController.mineBlock);
+// router.post('/mineBlock', P2PController.mineBlock);
+
+module.exports = {router, p2pServer}
