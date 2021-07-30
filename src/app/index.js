ec93ec9ebbb2b65f87b0e6b1dced142d43f6e9d9
Matheus Oliveira <62664418+MathSilms@users.noreply.github.com>
2021-07-30 17:54:06 -0300

    importando routes e p2p server para o index para rodar eles

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
-// const blockchain = require('../blockchain');
-const router = require('./routes/routes');
+// const Blockchain = require('../blockchain');
+const router = require('./routes/routes').router;
+const p2pServer = require('./routes/routes').p2pServer;
 const port = process.env.PORT ||3001;
 
 const app = express();
-//const bc = new Blockchain();
+// const bc = new Blockchain();
+
 app.use(router);
 app.use(express.json());
 
@@ -12,5 +14,9 @@ app.listen(port, ()=>{
     console.log('Servidor rodando na porta :', port)
 });
 
+p2pServer.listen();
+
+// dados para conectar com a segunda instância da blockchain
+//P2P_PORT=5002 PORT=3002 PEERS=ws://localhost:5001
 
 
